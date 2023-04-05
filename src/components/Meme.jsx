65e7f00241ee12415fa215addc60a5b0af836bb0
Jovy Cortez <jovycortez11@gmail.com>
2023-04-05 15:37:55 -0400

    function to generate random meme images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,25 +1,36 @@
-function Meme() {
-    return(
-        <div className="meme">
-           <main>
-                <form className="form">
-                    <input 
-                        type="text"
-                        placeholder="Top text"
-                        className="form--input"
-                    />
-                    
-                    <input 
-                        type="text"
-                        placeholder="Bottom text"
-                        className="form--input"
-                    />
-                    <button className="form--button">Get a new meme image 🖼 </button>
-                </form>
-           </main>
+import memesData from "../memesData";
+import { useState } from "react";
 
+function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+    console.log(memeImage);
+  }
+
+  return (
+    <div className="meme">
+      <main>
+        <div className="form">
+          <input type="text" id="topText" placeholder="Top text" className="form--input" />
+
+          <input
+            type="text"
+            id="bottomText"
+            placeholder="Bottom text"
+            className="form--input"
+          />
+          <button onClick={getMemeImage} className="form--button">
+            Get a new meme image 🖼
+          </button>
         </div>
-    )
+        <img src={memeImage} className="meme--image" alt={memeImage} />
+      </main>
+    </div>
+  );
 }
 
-export default Meme
+export default Meme;
