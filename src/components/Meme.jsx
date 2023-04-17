b310de2e9526051bfd3c5ea079c6f67124aef34f
Jovy Cortez <jovycortez11@gmail.com>
2023-04-17 18:02:16 -0400

    Adding API call from imgflip meme generator API

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,67 +1,68 @@
-import memesData from "../memesData";
-import { useState } from "react";
+import { useState, useEffect } from "react"
 
-function Meme() {
-  const [meme, setMeme] = useState({
-    topText: "",
-    bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg",
-  });
-  const [allMemeImages, setAllMemeImages] = useState(memesData);
-
-  function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    setMeme((prevMeme) => ({
-      ...prevMeme,
-      randomImage: url,
-    }));
-  }
-
-  function handleChange(event){
-    const {name, value} = event.target
+export default function Meme() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemes, setAllMemes] = useState([])
     
-      setMeme(prevMeme => ({
-        ...prevMeme,
-        [name]: value
-      }))
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
-  }
-
-  return (
-      <main>
-        <div className="form">
-          <input
-            type="text"
-            id="topText"
-            placeholder="Top text"
-            className="form--input"
-            value={meme.topText}
-            onChange={handleChange}
-            name="topText"
-          />
-
-          <input
-            type="text"
-            id="bottomText"
-            placeholder="Bottom text"
-            className="form--input"
-            value={meme.bottomText}
-            onChange={handleChange}
-            name="bottomText"
-          />
-          <button onClick={getMemeImage} className="form--button">
-            Get a new meme image 🖼
-          </button>
-        </div>
-        <div className="meme">
-          <img src={meme.randomImage} className="meme--image" alt={meme.randomImage} />
-          <h2 className="meme--text top">{meme.topText}</h2>
-          <h2 className="meme--text bottom">{meme.bottomText}</h2>
-        </div>
-      </main>
-  );
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" alt={meme.randomImage}/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
 }
-
-export default Meme;
